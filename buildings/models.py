a2736f61a0646a320006b9133c00afd98e5c41f5
ananthanandanan <ananthanandanan@gmail.com>
2022-12-04 23:28:44 +0530

    feat: Add Activity model

--- buildings/models.py
+++ buildings/models.py
@@ -151,3 +151,28 @@ class MaintenanceTicket(models.Model):
 
     def __str__(self):
         return f"{self.maintenance.maintenance_name} X {self.count}"
+
+
+class Activity(models.Model):
+    id = models.AutoField(primary_key=True)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    items = models.ManyToManyField(
+        Item, related_name="activity_items", blank=True, through="ActivityItem"
+    )
+    comments = models.TextField()
+    time = models.TimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Activities"
+
+    def __str__(self):
+        return str(self.id) + "#" + str(self.ticket.ticket_no)
+
+
+class ActivityItem(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    items = models.ForeignKey(Item, on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.items.item_name} X {self.count}"
