a35112f045e48fca5f65d82f614d88c0490b49cd
ananthanandanan <ananthanandanan@gmail.com>
2022-12-07 19:38:47 +0530

    feat: add ticket more detail view - Added extra filters for models - More detail new for ticket - Hide Activity side-panel for customers

--- members/urls.py
+++ members/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login_member/", views.login_member, name="login"),
     path("logout_member/", views.logout_member, name="logout"),
     path("register_member/", views.register_member, name="register"),
+    path("detail_ticket/<int:ticket_id>/", views.detail_ticket, name="detail_ticket"),
 ]
