9658fece1d5cd2dea6ee6aa7474c7815d5f8859a
ananthanandanan <ananthanandanan@gmail.com>
2022-12-10 22:25:40 +0530

    feat: change password form, view, fix register page

--- members/urls.py
+++ members/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path("agent/", views.agent, name="agent"),
     path("customer/", views.customer, name="customer"),
-    path("login_member/", views.login_member, name="login"),
-    path("logout_member/", views.logout_member, name="logout"),
-    path("register_member/", views.register_member, name="register"),
+    path("member_login/", views.login_member, name="login"),
+    path("member_logout/", views.logout_member, name="logout"),
+    path("register/", views.register_member, name="register"),
     path("detail_ticket/<int:ticket_id>/", views.detail_ticket, name="detail_ticket"),
+    path("change_password/", views.change_password, name="change_password"),
 ]
