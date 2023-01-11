9ab542bc91c34426d02fbcc6c7ac6640f74cf603
rishuriya <rdrishav2002@yahoo.com>
2023-01-11 18:58:53 +0530

    Feat: Added api for ticket, Dept and rooms

--- buildings/urls.py
+++ buildings/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("buildings/", views.index, name="index"),
+    path("tickets_get/", views.getTicket, name="getTicket"),
+    path("tickets_post/", views.postTicket, name="postTicket"),
+    path("get_department/", views.getDepartment, name="department"),
+    path("get_room/", views.getRoom, name="room"),
 ]
--- buildings/views.py
+++ buildings/views.py
@@ -3,11 +3,38 @@ from django.utils import timezone
 from django.views.generic.list import ListView
 from buildings.models import Building
 from django.contrib import admin
-
+from .models import Ticket, Department,Room
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializer import *
 
 def index(request):
     return render(request, "index.html")
 
+@api_view(['POST'])
+def postTicket(request):
+    serializer = TicketSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getTicket(request):
+    ticket = Ticket.objects.all()
+    serializer = TicketSerializer(ticket, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def getDepartment(request):
+    department = Department.objects.all()
+    serializer = DepartmentSerializer(department, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request):
+    room = Room.objects.all()
+    serializer = RoomSerializer(room, many=True)
+    return Response(serializer.data)
 
 class EventAdminSite(admin.AdminSite):
     def get_app_list(self, request):
