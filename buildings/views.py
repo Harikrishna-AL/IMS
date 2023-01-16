d7ced7d500cb0dc5da486ae672f5700b8311809f
rishuriya <rdrishav2002@yahoo.com>
2023-01-16 22:25:59 +0530

    fix: Changes APIs for integrating with qr-scanner

--- buildings/views.py
+++ buildings/views.py
@@ -3,44 +3,49 @@ from django.utils import timezone
 from django.views.generic.list import ListView
 from buildings.models import Building
 from django.contrib import admin
-from .models import Ticket, Department, Room
+from members.models import Members
+from .models import Ticket, Department,Room
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializer import *
 
-
 def index(request):
     return render(request, "index.html")
 
-
-@api_view(["POST"])
+@api_view(['POST'])
 def postTicket(request):
-    serializer = TicketSerializer(data=request.data)
+    value={}
+    department=[]
+    department.append(int(request.data['department']))
+    value['department']=department
+    value['room']=int(request.data['room'])
+    value['message']=request.data['message']
+    value["creator_name"]=request.data['patientName']
+    serializer = TicketSerializer(data=value)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
 
-
-@api_view(["GET"])
+@api_view(['GET'])
 def getTicket(request):
+    ticket_no = request.query_params.get('ticket_no')
     ticket = Ticket.objects.all()
-    serializer = TicketSerializer(ticket, many=True)
+    if ticket_no is not None:
+        ticket=ticket.filter(ticket_no=ticket_no)
+        serializer = TicketSerializer(ticket, many=True)
     return Response(serializer.data)
-
-
-@api_view(["GET"])
+    
+@api_view(['GET'])
 def getDepartment(request):
     department = Department.objects.all()
     serializer = DepartmentSerializer(department, many=True)
     return Response(serializer.data)
 
-
-@api_view(["GET"])
+@api_view(['GET'])
 def getRoom(request):
     room = Room.objects.all()
     serializer = RoomSerializer(room, many=True)
     return Response(serializer.data)
-
 
 class EventAdminSite(admin.AdminSite):
     def get_app_list(self, request):
